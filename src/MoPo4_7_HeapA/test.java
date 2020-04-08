313a8891634e33a71a55ff417dae489883430102
1194839756 <1194839756@qq.com>
2020-04-08 22:35:17 +0800

    Heap

---
+++ src/MoPo4_7_HeapA/test.java
@@ -0,0 +1,16 @@
+package MoPo4_7_HeapA;
+
+import java.util.PriorityQueue;
+
+public class test {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(7);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+        System.out.println(queue);
+    }
+}
