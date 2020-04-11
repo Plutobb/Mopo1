c2bdae7504915b4bfdb9bf1ea6380a9ad72c5718
1194839756 <1194839756@qq.com>
2020-04-11 18:58:10 +0800

    排序问题

--- src/MoPo4_10_Sort/test.java
+++ src/MoPo4_10_Sort/test.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class test {
     public static void main(String[] args) {
         int[] n = {5,6,3,4,8,7,2,10,1,9};
-        //System.out.println("冒泡排序");
-        //allSort.bubbleSort(n);
-        //allSort.inSort(n);
-        //allSort.shellSort(n);
-        //allSort.selectSort(n);
-        //allSort.heapSort(n);
-        //allSort.quickSort(n);
+        System.out.println("冒泡排序");
+        allSort.bubbleSort(n);
+        allSort.inSort(n);
+        allSort.shellSort(n);
+        allSort.selectSort(n);
+        allSort.heapSort(n);
+        allSort.quickSort(n);
         System.out.println(Arrays.toString(n));
     }
 }
