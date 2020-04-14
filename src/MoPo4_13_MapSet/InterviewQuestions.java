3c336233b0e709d07050da7d3dd8714af4387858
1194839756 <1194839756@qq.com>
2020-04-14 12:26:01 +0800

    面试问题---哈希表

--- src/MoPo4_13_MapSet/InterviewQuestions.java
+++ src/MoPo4_13_MapSet/InterviewQuestions.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class InterviewQuestions {
+    //题目1
     //只出现一次的数字;
     //给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
     public static int singleNumber(int[] nums) {
@@ -18,5 +19,41 @@ public class InterviewQuestions {
             }
         }
         return map.keySet().iterator().next();
+    }
+    //题目2
+    //    给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+    //
+    //    要求返回这个链表的 深拷贝。 
+    //
+    //    我们用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+    //
+    //    val：一个表示 Node.val 的整数。
+    //    random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+    static class  Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+    public static Node copyRandomList(Node head) {
+        Map<Node,Node> map = new HashMap<>();
+        //第一次循环将所有新节点与老节点一一对应;
+        for (Node cur = head; cur != null;cur = cur.next ){
+            map.put(cur,new Node(cur.val));
+        }
+        //第二次循环更新新节点的next和random节点;
+        //注意这里新节点指向的next和random是新节点的节点,而不是老节点的节点!
+        for (Node cur = head; cur != null; cur = cur.next){
+            Node newNode = map.get(cur);
+            //map.get(cur.next) 得到的是老节点(cur的next)作为<Key>对应作为<value>的新节点;
+            newNode.next = map.get(cur.next);
+            newNode.random = map.get(cur.random);
+        }
+        return map.get(head);
     }
 }
