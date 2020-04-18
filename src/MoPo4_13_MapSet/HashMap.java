15d17b42fa557e97ebcbc128bbab4c8d814642cb
1194839756 <1194839756@qq.com>
2020-04-18 19:14:14 +0800

    哈希表底层实现

--- src/MoPo4_13_MapSet/HashMap.java
+++ src/MoPo4_13_MapSet/HashMap.java
@@ -10,6 +10,8 @@ public class HashMap {
             this.value = value;
         }
     }
+    //负载因子;
+    private static final double LOAD_FACTOR = 0.75;
     //array数组就是哈希表的本体,数组每个元素又是链表的头结点;
     private Node[] array = new Node[101];
     private int size = 0;
@@ -35,5 +37,33 @@ public class HashMap {
         newNode.next = list;
         array[index] = newNode;
         size++;
+        //当元素过多,负载因子过大时需要扩容;
+        if (size / array.length > LOAD_FACTOR){
+            resize();
+        }
+    }
+    public void resize(){
+        Node[] newArray = new Node[array.length*2];
+        //将元素放到新的哈希表中;
+        for (int i = 0; i < array.length; i++) {
+            for (Node cur = array[i]; cur != null; cur = cur.next){
+                int index = cur.key % newArray.length;
+                Node newNode = new Node(cur.key,cur.value);
+                newNode.next = newArray[index];
+                newArray[index]  =newNode;
+            }
+        }
+        array = newArray;
+    }
+    //根据key找到对应的value,找到返回value,否则返回null;
+    public Integer get(int key){
+        int index = hashFunc(key);
+        Node list = array[index];
+        for (Node cur = list; cur != null; cur = cur.next){
+            if (cur.key == key){
+                return cur.value;
+            }
+        }
+        return null;
     }
 }
