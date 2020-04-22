a7ee2c0f5709fbc361d30cbe44426fd2c17ca364
1194839756 <1194839756@qq.com>
2020-04-22 21:57:22 +0800

    面试题---二叉树-祖先节点

--- src/MoPo_InterviewQuestion/TreeQuestion.java
+++ src/MoPo_InterviewQuestion/TreeQuestion.java
@@ -39,6 +39,22 @@ public class TreeQuestion {
         return ret;
     }
 
+//    给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+//
+//    百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root ==null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if (left == null) {
+            return right;
+        }
+        else if (right == null) {
+            return left;
+        }else {
+            return root;
+        }
+    }
     public static void main(String[] args) {
         TreeNode a = new TreeNode(0);
         TreeNode b = new TreeNode(1);
