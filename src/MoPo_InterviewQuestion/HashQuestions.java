24b3ddf1177f8182f5e66db4724c8b2d73f64763
1194839756 <1194839756@qq.com>
2020-04-29 22:21:11 +0800

    面试题---哈希表

--- src/MoPo_InterviewQuestion/HashQuestions.java
+++ src/MoPo_InterviewQuestion/HashQuestions.java
@@ -1,5 +1,6 @@
 package MoPo_InterviewQuestion;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class HashQuestions {
@@ -9,13 +10,29 @@ public class HashQuestions {
         int i = 0,ans = 0,j = 0;
         int n = s.length();
         while (i < n && j < n){
+            //当字符不存在的时候,插入到set中;
             if (!set.contains(s.charAt(j))){
                 set.add(s.charAt(j++));
+                //将set最大长度存放在ans中;
                 ans = Math.max(ans,j - i);
             }else {
+                //当字符存在就删除set头部位置的一个字符;
+                //当删除后还存在就继续删除,直到不存在;
                 set.remove(s.charAt(i++));
             }
         }
         return ans;
+    }
+    //找出数组中重复的数字。
+    public static int findRepeatNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])){
+                set.add(nums[i]);
+            }else {
+                return nums[i];
+            }
+        }
+        return -1;
     }
 }
