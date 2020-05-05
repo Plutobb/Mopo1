c488a1281cdbbc60cfe396e7ab09327e94cfbf01
1194839756 <1194839756@qq.com>
2020-05-05 23:56:41 +0800

    面试题---哈希表

--- src/MoPo_InterviewQuestion/HashQuestions.java
+++ src/MoPo_InterviewQuestion/HashQuestions.java
@@ -158,10 +158,19 @@ public class HashQuestions {
         }
         return ' ';
     }
+    //给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j
+    // 使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
 
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        String a = "abd";
-        String b = "abdd";
-        System.out.println(findTheDifference(a,b));
+//        String a = "abd";
+//        String b = "abdd";
+//        System.out.println(findTheDifference(a,b));
+        int[] nums = {1,0,1,1};
     }
 }
