5f142a79d9338db915cb58f5b9322543c6b04a56
1194839756 <1194839756@qq.com>
2020-05-06 18:57:58 +0800

    面试题---哈希表

--- src/MoPo_InterviewQuestion/HashQuestions.java
+++ src/MoPo_InterviewQuestion/HashQuestions.java
@@ -171,6 +171,20 @@ public class HashQuestions {
         }
         return false;
     }
+    //给定一个整数数组，判断是否存在重复元素。
+    //如果任意一值在数组中出现至少两次，函数返回 true。
+    // 如果数组中每个元素都不相同，则返回 false 。
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
 //        String a = "abd";
 //        String b = "abdd";
