be29bb8214f5a81457aed0867e6c9f3addc04a10
1194839756 <1194839756@qq.com>
2020-05-06 22:03:25 +0800

    面试题---哈希表

--- src/MoPo_InterviewQuestion/HashQuestions.java
+++ src/MoPo_InterviewQuestion/HashQuestions.java
@@ -1,9 +1,7 @@
 package MoPo_InterviewQuestion;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class HashQuestions {
     //给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -185,10 +183,47 @@ public class HashQuestions {
         }
         return false;
     }
+    //给定一个段落 (paragraph) 和一个禁用单词列表 (banned)。返回出现次数最多，同时不在禁用列表中的单词。
+    //题目保证至少有一个词不在禁用列表中，而且答案唯一。
+    //禁用列表中的单词用小写字母表示，不含标点符号。
+    // 段落中的单词不区分大小写。答案都是小写字母。
+
+    public static String mostCommonWord(String paragraph, String[] banned) {
+        HashMap<String,Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>(Arrays.asList(banned));
+        //加入" "避免出现.后跟空字符的情况;
+        set.add("");
+        paragraph = paragraph.toLowerCase();
+        paragraph += '.';
+        //结果单词
+        String ans = "";
+        //记录出现的最大次数
+        int maxTime = 0;
+        int i = 0;
+        for (int j = 0; j < paragraph.length(); j++) {
+            char c = paragraph.charAt(j);
+            if (c >= 'a' &&  c <= 'z'){
+                continue;
+            }else {
+                String word = paragraph.substring(i,j);
+                if (!set.contains(word)){
+                    map.put(word,map.getOrDefault(word,0)+1);
+                    if (map.get(word) > maxTime){
+                        maxTime = map.get(word);
+                        ans = word;
+                    }
+                }
+            }
+            i = j + 1;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
 //        String a = "abd";
 //        String b = "abdd";
 //        System.out.println(findTheDifference(a,b));
-        int[] nums = {1,0,1,1};
+        String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        String[] ban ={"hit"};
+        System.out.println(mostCommonWord(paragraph,ban));
     }
 }
