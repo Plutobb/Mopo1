73c1b0150e343af3a1fd5632e79224616bf61849
1194839756 <1194839756@qq.com>
2020-05-10 22:33:39 +0800

    面试题---哈希表

--- src/MoPo_InterviewQuestion/HashQuestions.java
+++ src/MoPo_InterviewQuestion/HashQuestions.java
@@ -290,6 +290,33 @@ public class HashQuestions {
             return index;
         }
     }
+    //给定一个保存员工信息的数据结构，它包含了员工唯一的id，重要度 和 直系下属的id。
+    //比如，员工1是员工2的领导，员工2是员工3的领导。他们相应的重要度为15, 10, 5。
+    // 那么员工1的数据结构是[1, 15, [2]]，员工2的数据结构是[2, 10, [3]]，
+    // 员工3的数据结构是[3, 5, []]。注意虽然员工3也是员工1的一个下属，
+    // 但是由于并不是直系下属，因此没有体现在员工1的数据结构中。
+    //现在输入一个公司的所有员工信息，以及单个员工id，返回这个员工和他所有下属的重要度之和。
+    class Employee {
+        public int id;
+        public int importance;
+        public List<Integer> subordinates;
+    };
+    public static int getImportance(List<Employee> employees, int id) {
+        HashMap<Integer,Employee> map = new HashMap<>();
+        for (Employee emp : employees){
+            map.put(emp.id,emp);
+        }
+        return getImp(map,id);
+    }
+    public static int getImp(HashMap<Integer,Employee> map,int id){
+        Employee employee = map.get(id);
+        int ret = employee.importance;
+        for (int i = 0; i < employee.subordinates.size(); i++) {
+            int subId = employee.subordinates.get(i);
+            ret += getImp(map,subId);
+        }
+        return ret;
+    }
     public static void main(String[] args) {
         String a = "ddc";
 //        String b = "abc";
