2ba3f6e8902e594766b351973301f00bc8f121d6
1194839756 <1194839756@qq.com>
2020-05-12 22:01:30 +0800

    面试题---堆栈

--- src/MoPo_InterviewQuestion/StackQuestion.java
+++ src/MoPo_InterviewQuestion/StackQuestion.java
@@ -1,5 +1,7 @@
 package MoPo_InterviewQuestion;
 
+import sun.misc.Queue;
+
 import java.util.HashSet;
 import java.util.Stack;
 
@@ -42,9 +44,29 @@ public class StackQuestion {
         }
         return num.pop();
     }
-
+    //输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。
+    // 假设压入栈的所有数字均不相等。
+    // 例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列，
+    // 但4,3,5,1,2就不可能是该压栈序列的弹出序列。（注意：这两个序列的长度是相等的）
+    public static boolean IsPopOrder(int [] pushA,int [] popA) {
+        if (pushA.length == 0 || popA.length == 0 || popA.length != pushA.length)
+            return false;
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+        for (int i = 0; i < pushA.length; i++) {
+            stack.push(pushA[i]);
+            while (!stack.isEmpty() && stack.peek() == popA[j]){
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
     public static void main(String[] args) {
-        String[] a = {"2","3","+","3","*","3","-"};
-        System.out.println(evalRPN(a));
+//        String[] a = {"2","3","+","3","*","3","-"};
+        int[] a = {1,2,3,4,5};
+        int[] b = {4,5,3,2,1};
+        System.out.println(IsPopOrder(a,b));
+//        System.out.println(evalRPN(a));
     }
 }
