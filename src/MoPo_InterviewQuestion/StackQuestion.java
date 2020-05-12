c6f420e91e59022eca39d12c6381d8044a86b757
1194839756 <1194839756@qq.com>
2020-05-12 22:22:34 +0800

    面试题---堆栈

--- src/MoPo_InterviewQuestion/StackQuestion.java
+++ src/MoPo_InterviewQuestion/StackQuestion.java
@@ -76,9 +76,9 @@ public class StackQuestion {
         for(String op : ops) {
             if (op.equals("+")) {
                 int top = stack.pop();
-                int newtop = top + stack.peek();
+                int newTop = top + stack.peek();
                 stack.push(top);
-                stack.push(newtop);
+                stack.push(newTop);
             } else if (op.equals("C")) {
                 stack.pop();
             } else if (op.equals("D")) {
@@ -90,6 +90,23 @@ public class StackQuestion {
         int ans = 0;
         for(int score : stack) ans += score;
         return ans;
+    }
+    //给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，
+    // 判断二者是否相等，并返回结果。 # 代表退格字符。
+    //注意：如果对空文本输入退格字符，文本继续为空。
+    public static boolean backspaceCompare(String S, String T) {
+        //判断是否两个栈相同,艺术的方法;
+        return build(S).equals(build(T));
+    }
+    public static String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
     }
     public static void main(String[] args) {
 //        String[] a = {"2","3","+","3","*","3","-"};
