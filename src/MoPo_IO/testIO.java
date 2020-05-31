039e323d2a19e2bfaeb1ba3a4ce7f4987fcd4f88
1194839756 <1194839756@qq.com>
2020-05-31 22:58:57 +0800

    IO

--- src/MoPo_IO/testIO.java
+++ src/MoPo_IO/testIO.java
@@ -6,7 +6,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class testJava {
+public class testIO {
     @Test
     public void t1(){
         File f1 = new File("");
