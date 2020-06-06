4ed19313b3632532e1156da6fbc60eef6dbf040e
Plutobb <1194839756@qq.com>
2020-06-06 16:23:59 +0800

    动态规划法

--- src/MoPo_ALG/test.java
+++ src/MoPo_ALG/test.java
@@ -1,5 +1,7 @@
 package MoPo_ALG;
 
+import java.util.Scanner;
+
 public class test {
     public static void help(int x){
         if (x <= 0){
@@ -11,8 +13,27 @@ public class test {
         help(x);
         System.out.println(i);
     }
+    public static void dongTai(){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] a = new int[n + 1];
+        int[][] dp = new int[n + 1][41];// dp[i][j]表示从前i种物品拿出体积j的方法数
+        for (int i = 1; i <= n; i++) {
+            a[i] = s.nextInt();
+            dp[i][0] = 1;//如果让拿的物品总重量为0，则有一种方法，即什么也不拿
+        }
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 40; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= a[i])
+                    dp[i][j] += dp[i - 1][j - a[i]];
+            }
+        }
+        System.out.println(dp[n][40]);
+    }
 
     public static void main(String[] args) {
-        help(100);
+        dongTai();
     }
 }
